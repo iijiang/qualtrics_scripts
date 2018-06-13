05ecfaf29c5a0ffe5f40d887be00d2477d3afadb
Yi Jiang <yijiang@Yis-MBP-2.home>
2018-06-13 15:55:18 +1000

    Postmessage example

---
+++ qualtricsPostMessage.js
@@ -0,0 +1,15 @@
+ /*Use the "script" opening and closing tags only if you are placing this script in the header of your survey(to run on all pages)*/
+
+Qualtrics.SurveyEngine.addOnload(function()  { 
+
+  // Wait half a second and then adjust the iframe height to fit the questions
+
+  setTimeout(function () {
+    var height = document.getElementById("Wrapper").scrollHeight-100;
+    console.log(height);
+
+   //parent.postMessage( document.getElementById("Wrapper").scrollHeight+"px","http://employment.loc.gbuild.net");
+   parent.postMessage(height+"px", document.referrer.match(/^.+:\/\/[^\/]+/)[0]);
+
+  }, 500);
+});
