2bf5087b3be90c1b779cf89b1abfc86339678656
Yi Jiang <yijiang@Yis-MBP-2.home>
2018-06-14 12:47:00 +1000

    Fixed indentation

--- hideNextbuttonClickToNextKeyboardAccessible_example.js
+++ hideNextbuttonClickToNextKeyboardAccessible_example.js
@@ -1,32 +1,25 @@
-Qualtrics.SurveyEngine.addOnload(function()
-{
+Qualtrics.SurveyEngine.addOnload(function() {
 	this.hideNextButton();
   //this.hidePreviousButton();
 
 	var that = this;
 
-	this.questionclick = function(event,element)
-	{
+	this.questionclick = function(event,element) {
 		//by default you get the click event as the first parameter and the clicked element as the second parameter
 		//console.log(event);
 		//console.log(element)
-		if (element.type == 'radio')
-		{
+		if (element.type == 'radio') {
 			var that = this;
 			if (event.clientX > 0) {
 				(function(){that.clickNextButton();}).delay(1);
-
 			} else {
-
 				Event.observe(document, 'keydown', function keydownCallback(e) {
-
-        if (e.keyCode == 13 || e.keyCode == 32) {
-          Event.stopObserving(document, 'keydown', keydownCallback);
-          that.clickNextButton();
-        }
-
+				if (e.keyCode == 13 || e.keyCode == 32) {
+					Event.stopObserving(document, 'keydown', keydownCallback);
+					that.clickNextButton();
+					}
 				});
-			}
+		  }
 		}
 	}
 });
