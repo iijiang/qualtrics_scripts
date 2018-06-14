d37f4379e00942cb3d2a87f105499a81345a62de
Yi Jiang <yijiang@Yis-MBP-2.home>
2018-06-14 14:41:39 +1000

    Fixing

--- hideNextbuttonClickToNextKeyboardAccessible_example.js
+++ hideNextbuttonClickToNextKeyboardAccessible_example.js
@@ -1,25 +1,23 @@
 Qualtrics.SurveyEngine.addOnload(function() {
-	this.hideNextButton();
+  this.hideNextButton();
   //this.hidePreviousButton();
-
-	var that = this;
-
-	this.questionclick = function(event,element) {
-		//by default you get the click event as the first parameter and the clicked element as the second parameter
-		//console.log(event);
-		//console.log(element)
-		if (element.type == 'radio') {
-			var that = this;
-			if (event.clientX > 0) {
-				(function(){that.clickNextButton();}).delay(1);
-			} else {
-				Event.observe(document, 'keydown', function keydownCallback(e) {
-				if (e.keyCode == 13 || e.keyCode == 32) {
-					Event.stopObserving(document, 'keydown', keydownCallback);
-					that.clickNextButton();
-					}
-				});
-		  }
-		}
-	}
+  var that = this;
+  this.questionclick = function(event,element) {
+  //by default you get the click event as the first parameter and the clicked element as the second parameter
+  //console.log(event);
+  //console.log(element)
+  if (element.type == 'radio') {
+    var that = this;
+    if (event.clientX > 0) {
+      (function(){that.clickNextButton();}).delay(1);
+    } else {
+     Event.observe(document, 'keydown', function keydownCallback(e) {
+       if (e.keyCode == 13 || e.keyCode == 32) {
+         Event.stopObserving(document, 'keydown', keydownCallback);
+         that.clickNextButton();
+       }
+     });
+    }
+    }
+  }
 });
